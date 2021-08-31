b2c783107713ce6728e833b41f4677d69d2166b9
bhavana280699 <balabhavana.k@msitprogram.net>
2021-08-31 20:38:29 +0530

    done

--- 04-recursion_powersof3ton-Python/recursion_powersof3ton.py
+++ 04-recursion_powersof3ton-Python/recursion_powersof3ton.py
@@ -3,6 +3,23 @@
 # positive powers of 3 up to and including n. As an example, powersOf3ToN(10.5) returns [1, 3, 9]. If no such powers 
 # of 3 exist, you should return the empty list. You may not use loops/iteration in this problem. 
 
+def wrapper(l1,l2):
+    empty=[]
+    final=3**l2
+    if(final<=l1):
+        empty.append(final)
+        return empty+wrapper(l1,l2+1)
+    else:
+        return empty
+    
 def recursion_powersof3ton(n):
-	# Your code goes here
-	pass
+  k=int(n)
+  empty=[]
+  if(k<=0):
+    return None
+  if(n==1):
+    empty.append(k)
+    return empty
+  else:
+    return wrapper(k,0)
+
