19575a18de4e89708338a0024ff6824b7629a2bc
bhavana280699 <balabhavana.k@msitprogram.net>
2021-08-31 20:11:35 +0530

    done

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -7,6 +7,41 @@
 # so fun_nthsmithnumber(0) should return 4
 # so fun_nthsmithnumber(1) should return 22
 
+import math
+
+def isprime(n):
+    if(n==2):
+        return True
+    elif(n%2==0 or n<2):
+        return False
+    for i in range(3,int(math.sqrt(n))+1,2):
+        if(n%i==0):
+            return False
+    return True
+    
+def SumOfDigits(n):
+    if(n<10):
+        return n
+    else:
+        return n%10+SumOfDigits(n//10)
+
+def issmith(n):
+    Digits=SumOfDigits(n)
+    Factors=0
+    i=2
+    while(i*i<=n):
+        if(n%i!=0):
+            i+=1
+        else:
+            n//=i
+            Factors+=SumOfDigits(i)
+    return Digits==Factors+SumOfDigits(n)
 
 def fun_nth_smithnumber(n):
-    return 1
+    found=-1
+    got=0
+    while(found<=n):
+        got+=1
+        if(issmith(got) and isprime(got)==False):
+            found+=1
+    return got
