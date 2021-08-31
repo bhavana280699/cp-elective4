758e1f1f030eefb5fcfd2f9e7aaa99c019454227
bhavana280699 <balabhavana.k@msitprogram.net>
2021-08-31 20:42:43 +0530

    done

--- set.py
+++ set.py
@@ -7,6 +7,29 @@
 # 	limitedPowerSet(5, 7) => 
 # [ {}, {1}, {2}, {3}, {4}, {5}, {1, 2} ]
 
-def limitedPowerSet(n, k):
-    # Your code goes here...
-    pass
+def limitedPowerSet(n,l):
+    k=[]
+    k.append({})
+    for i in range(1,n+1):
+        for j in range(1,(n+1)-(i-1)):
+            temp=[]
+            for x in range(j,j+i):
+                temp.append(x)
+                
+            if(len(k)<l):
+                k.append(set(temp))
+            else:
+                return k
+    if(len(k)==l):
+        return k
+    else:
+        return None
+    
+    
+print(limitedPowerSet(5,16))
+print(limitedPowerSet(0,3))
+print(limitedPowerSet(0,1))
+print(limitedPowerSet(5,3))
+print(limitedPowerSet(3,3))
+print(limitedPowerSet(7,17))
+print("All Test cases passed................")
